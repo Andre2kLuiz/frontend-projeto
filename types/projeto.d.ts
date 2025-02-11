50906ee00224afab6e16d77a2c75accefa072f98
Andre Luiz <andre2kluiz@gmail.com>
2025-02-11 11:27:31 -0300

    prieiro commit do frontend-projeto

---
+++ types/projeto.d.ts
@@ -0,0 +1,10 @@
+declare namespace Projeto {
+    type Usuario = {
+        length: number;
+        id?: number;
+        nome?: string;
+        login?: string;
+        senha?: string;
+        email?: string;
+    };
+}
